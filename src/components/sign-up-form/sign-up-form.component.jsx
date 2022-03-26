0314dda62027bf1fcf754a970557f417efec1053
matthew <mmcampbell0@gmail.com>
2022-03-26 10:32:09 -0400

    created auth page component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -54,7 +54,7 @@ const SignUpForm = () => {
                 <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email}/>
                 <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password}/>
                 <FormInput label="Confirm Password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
-                <Button buttonType={"google"} type="submit">Sign Up</Button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
